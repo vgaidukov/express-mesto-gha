1b406ac738f96f50841a13979b75e69c6b9cfdcc
Vadim Gaidukov <vgaidukov@gmail.com>
2022-09-08 23:28:39 +0300

    feat: app prepare, mogoose init

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+
+mongoose.connect('mongodb://localhost:27017/mestodb');
+
+app.listen(3000, () => {
+  console.log('Сервер запущен');
+})
