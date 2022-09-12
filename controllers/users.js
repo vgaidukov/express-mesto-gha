600902db2a1da7224cc80dc9920e71f271595853
Vadim Gaidukov <vgaidukov@gmail.com>
2022-09-12 19:45:29 +0300

    fix: return in errorHandler

--- controllers/users.js
+++ controllers/users.js
@@ -1,9 +1,16 @@
+const bcrypt = require('bcryptjs');
 const User = require('../models/user');
 const { checkIdValidity } = require('../utils/checkIdValidity');
 const { setErrorType } = require('../utils/setErrorType');
 
 const ValidationError = require('../utils/errors/ValidationError');
 const CastError = require('../utils/errors/CastError');
+
+async function findByEmail(e) {
+  const isExist = await User.findOne({ email: e.email })
+    .then((user) => user);
+  return isExist;
+}
 
 const getUsers = (req, res, next) => {
   User.find({})
@@ -28,14 +35,35 @@ const getUser = (req, res, next) => {
 };
 
 const createUser = (req, res, next) => {
-  const { name, about, avatar } = req.body;
-  User.create({ name, about, avatar })
-    .then((user) => {
-      res.send(user);
+  const {
+    name,
+    about,
+    avatar,
+    email,
+  } = req.body;
+  findByEmail({ email })
+    .then((isExist) => {
+      if (!isExist) {
+        bcrypt.hash(req.body.password, 10)
+          .then((hash) => User.create({
+            name,
+            about,
+            avatar,
+            email,
+            password: hash,
+          }))
+          .then((user) => {
+            res.send(user);
+          })
+          .catch((err) => {
+            next(setErrorType(err));
+          });
+      } else {
+        const validationError = new ValidationError();
+        next(validationError);
+      }
     })
-    .catch((err) => {
-      next(setErrorType(err));
-    });
+    .catch(next);
 };
 
 const setUserInfo = (req, res, next) => {
